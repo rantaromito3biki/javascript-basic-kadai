97c4ba9719fe022be3fd3e169bd4f1233b53d30f
rantaromito3biki <yukimi.daifuku.0529@gmail.com>
2024-11-06 14:54:52 +0900

    Delete kadai_-020 directory

--- kadai_-020/event.js
+++
@@ -1,8 +0,0 @@
-//ボタンとテキスト要素を取得
-const button = document.getElementById("btn");
-const text = document.getElementById("text");
-
-//ボタンがクリックされたときの処理
-button.addEventListener("click", () => {
-    text.textContent = "ボタンをクリックしました";
-});
